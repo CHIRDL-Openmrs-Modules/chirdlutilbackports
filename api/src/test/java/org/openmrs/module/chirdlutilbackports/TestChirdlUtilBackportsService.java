a948019d6d72f6084632db714b739941497d2426
davely <davely@IN-PEDS-01736.ads.iu.edu>
2018-06-27 12:10:35 -0400

    Added a couple of unit tests

--- api/src/test/java/org/openmrs/module/chirdlutilbackports/TestChirdlUtilBackportsService.java
+++ api/src/test/java/org/openmrs/module/chirdlutilbackports/TestChirdlUtilBackportsService.java
@@ -4,11 +4,14 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openmrs.annotation.Authorized;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.chirdlutilbackports.hibernateBeans.LocationTagAttribute;
+import org.openmrs.module.chirdlutilbackports.hibernateBeans.LocationTagAttributeValue;
 import org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState;
 import org.openmrs.module.chirdlutilbackports.service.ChirdlUtilBackportsService;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
@@ -31,7 +34,7 @@ public class TestChirdlUtilBackportsService extends BaseModuleContextSensitiveTe
 		    }
 		}
 	}
-
+	
 	@Test
 	public void testGetPatientStatesByFormNameAndState() throws Exception {
 		executeDataSet("dbunitFiles/patientStates.xml");
@@ -69,5 +72,76 @@ public class TestChirdlUtilBackportsService extends BaseModuleContextSensitiveTe
 		Assert.assertEquals("Number of patient state rows did not match", 1, patientStates.size());
 		patientStates.clear();
 		stateNames.clear();	
+	}
+	
+	/**
+     * Tests deleting a chirdlutilbackports_location_tag_attribute_value record
+     * 
+     * @see org.openmrs.module.chirdlutilbackports.service.ChirdlUtilBackportsService#getEncounterAttributeValueByValue(String, String)
+     * 
+     * @throws Exception
+     */
+	@Test
+	public void should_getEncounterAttributeValueByValue() throws Exception{
+	    
+	}
+	
+	/**
+     * Tests deleting a chirdlutilbackports_location_tag_attribute_value record
+     * 
+     * @see org.openmrs.module.chirdlutilbackports.service.ChirdlUtilBackportsService#getPatientStatesBySessionId(Integer, List, boolean)
+     * 
+     * @throws Exception
+     */
+	@Test
+	public void should_getPatientStatesBySessionId() throws Exception{
+	    executeDataSet("dbunitFiles/patientStates.xml");
+        Context.authenticate("user1", "testpassword");
+        
+        ChirdlUtilBackportsService chirdlUtilBackportsService = Context.getService(ChirdlUtilBackportsService.class);
+        List<String> stateNames = new ArrayList<>();
+        String stateNameStr = "TEST STATE";
+        stateNames.add(stateNameStr);
+        
+        // One record is retired and one is not
+        Map<String, List<PatientState>> stateMap = chirdlUtilBackportsService.getPatientStatesBySessionId(23190, stateNames, false);
+        List<PatientState> patientStates = stateMap.get(stateNameStr);
+        Assert.assertEquals("Number of patient state rows did not match in should_getPatientStatesBySessionId()", 1, patientStates.size());
+        
+        // Include retired
+        stateMap.clear();
+        patientStates.clear();
+        stateMap = chirdlUtilBackportsService.getPatientStatesBySessionId(23190, stateNames, true);
+        patientStates = stateMap.get(stateNameStr);
+        Assert.assertEquals("Number of patient state rows did not match in should_getPatientStatesBySessionId()", 2, patientStates.size());
+	}
+	
+	/**
+	 * Tests deleting a chirdlutilbackports_location_tag_attribute_value record
+	 * 
+	 * @see org.openmrs.module.chirdlutilbackports.service.ChirdlUtilBackportsService#deleteLocationTagAttributeValueByValue(LocationTagAttribute, String)
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void should_deleteLocationTagAttributeValueByValue() throws Exception{
+	    executeDataSet("dbunitFiles/locationTagAttributeValues.xml");
+        Context.authenticate("user1", "testpassword");
+        
+        String locationTagStr = "TestAttribute";
+        ChirdlUtilBackportsService chirdlUtilBackportsService = Context.getService(ChirdlUtilBackportsService.class);
+        LocationTagAttribute locationTagAttribute = chirdlUtilBackportsService.getLocationTagAttribute(locationTagStr);
+        LocationTagAttributeValue locationTagAttributeValue = chirdlUtilBackportsService.getLocationTagAttributeValue(2987, locationTagStr, 8992);
+        
+        // Delete the record
+        chirdlUtilBackportsService.deleteLocationTagAttributeValueByValue(locationTagAttribute, "TestValue");
+        
+        // Query again and make sure the object is null
+        locationTagAttributeValue = chirdlUtilBackportsService.getLocationTagAttributeValue(2987, locationTagStr, 8992);
+        Assert.assertNull("Location tag attribute value was not deleted.", locationTagAttributeValue);
+        
+        // Make sure this record still exits
+        locationTagAttributeValue = chirdlUtilBackportsService.getLocationTagAttributeValue(2988, locationTagStr, 8992);
+        Assert.assertNotNull("Location tag attribute value was not found.", locationTagAttributeValue);
 	}
 }
